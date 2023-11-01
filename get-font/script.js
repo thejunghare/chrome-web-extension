b04e8d38038508eaf26dbf87186356d97518bd8b
Prasad Junghare <86673407+thejunghare@users.noreply.github.com>
2023-11-01 17:23:16 +0530

    Update script.js

--- get-font/script.js
+++ get-font/script.js
@@ -1,25 +1,48 @@
 'use strict'
-
-const getfont = (element, property) => {
-    return window
-        .getComputedStyle(element, null)
-        .getPropertyValue(property)
-}
-
-const getId = (obj) => {
-    return obj.id
-}
-
-addEventListener("mouseover", (event) => {
-    console.log(getId(event.target));
-})
-
-addEventListener("mouseover", (event) => {
-    //console.log(getfont(getId(event.target), "font-family")) // problem with this is I am passig id  instead of element
-
-    const element = event.target
-    const id = getId(element)
-    if (id){
-        console.log(getfont(element, "font-family"))
+document.addEventListener("DOMContentLoaded", function () {
+    const getfont = (element, property) => {
+        return window
+            .getComputedStyle(element, null)
+            .getPropertyValue(property)
     }
+
+    const getId = (obj) => {
+        return obj.id
+    }
+
+    const addTooltip = (element) => {
+        console.log(`added`);
+        element.setAttribute("data-bs-toggle", "tooltip")
+        element.setAttribute("data-bs-placement", "bottom")
+        element.setAttribute("data-bs-title", "Tooltip on top")
+        new bootstrap.Tooltip(element)
+
+        // Remove the tooltip when the mouse leaves the element
+        element.addEventListener("mouseleave", () => {
+            console.log(`Removed`);
+            // Remove the attributes to disable the tooltip
+            element.removeAttribute("data-bs-toggle");
+            element.removeAttribute("data-bs-placement");
+            element.removeAttribute("data-bs-title");
+
+            // Dispose of the tooltip
+           // tooltip.dispose();
+        })
+    }
+
+
+    addEventListener("mouseover", (event) => {
+        console.log(getId(event.target));
+    })
+
+    addEventListener("mouseover", (event) => {
+        //console.log(getfont(getId(event.target), "font-family")) // problem with this is I am passig id  instead of element
+
+        const element = event.target
+        const id = getId(element)
+        if (id) {
+            addTooltip(element)
+            console.log(getfont(element, "font-family"))
+        }
+    })
 })
